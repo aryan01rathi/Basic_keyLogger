8d10de64fb2c88331f892c2fe88f3c6019815e97
Aryan Rathi <aryan2001rathi@gmail.com>
2025-02-12 17:09:50 +0530

    Display pressed keys

---
+++ trial1_keylogger.py
@@ -0,0 +1,24 @@
+# Program to show what keys are pressed. It is stopped by CTRL+C
+from pynput.keyboard import Listener
+import time
+
+def on_press(key):
+    try:
+        print(f"Key {key.char} pressed")
+    except AttributeError:
+        print(f"{key} is pressed")
+
+# Start the listener
+listener = Listener(on_press=on_press)
+listener.start()
+
+try:
+    # we didn't use join because it would have blocked the main thread and keyboardinterrupt will not work
+    while listener.running:  
+        pass
+except KeyboardInterrupt:
+    print("\nStopped by user (Ctrl + C)")
+    # stops the execution of listener
+    listener.stop()  
+    # exits the thread
+    listener.join()
